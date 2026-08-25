f2bc766bea6e7d92acff370941c22801d9c637b6
notabotnetrepotrust <hailbotnet@proton.me>
2026-08-25 14:39:25 -0500

    Create bob.c

---
+++ bob.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+
+    time_t now = time(NULL);
+    struct tm *current = localtime(&now);
+
+    int bedtime_hour = 21;
+    int bedtime_minute = 30;
+
+    int current_minutes = current->tm_hour * 60 + current->tm_min;
+    int bedtime_minutes = bedtime_hour * 60 + bedtime_minute;
+
+    int difference = bedtime_minutes - current_minutes;
+
+    if (difference > 0) {
+
+        int hours = difference / 60;
+        int minutes = difference % 60;
+
+        printf("Bobs bedtime is in %d hours and %d minutes.\n",
+               hours, minutes);
+
+    } else if (difference == 0) {
+
+        printf("It's Bob's bedtime! Go to bed! mummy will get angry and turn off pc!\n");
+
+    } else {
+
+        printf("Bob is now dreaming about telnet\n");
+
+    }
+
+    return 0;
+}

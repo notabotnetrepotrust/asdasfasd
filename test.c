340d2214ba9a2155bb89ae6c8016f89b1b833b7c
notabotnetrepotrust <hailbotnet@proton.me>
2026-08-25 20:25:37 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 int main() {
 
-    // Force timezone to Netherlands, since that's where Bob lives
-    setenv("TZ", "Europe/Amsterdam", 1);
-    tzset();
-
     time_t now = time(NULL);
-    struct tm *current = localtime(&now);
+    struct tm utc = *gmtime(&now);
+
+    int year = utc.tm_year + 1900;
+    int offset = 1;
+
+    struct tm dst_start = {0}, dst_end = {0};
+    dst_start.tm_year = utc.tm_year; dst_start.tm_mon = 2; dst_start.tm_mday = 31;
+    dst_end.tm_year   = utc.tm_year; dst_end.tm_mon = 9;  dst_end.tm_mday = 31;
+    mktime(&dst_start); mktime(&dst_end);
+
+    dst_start.tm_mday -= dst_start.tm_wday;
+    dst_end.tm_mday   -= dst_end.tm_wday;
+    time_t dst_start_t = mktime(&dst_start);
+    time_t dst_end_t   = mktime(&dst_end);
+    time_t utc_t       = mktime(&utc);
+
+    if (utc_t >= dst_start_t && utc_t < dst_end_t) {
+        offset = 2;
+    }
+
+    time_t nl_time = now + offset * 3600;
+    struct tm *bob_time = gmtime(&nl_time);
 
     int bedtime_hour = 21;
     int bedtime_minute = 30;
 
-    int current_minutes = current->tm_hour * 60 + current->tm_min;
+    int current_minutes = bob_time->tm_hour * 60 + bob_time->tm_min;
     int bedtime_minutes = bedtime_hour * 60 + bedtime_minute;
 
     int difference = bedtime_minutes - current_minutes;
